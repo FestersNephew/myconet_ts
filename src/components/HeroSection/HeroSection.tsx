25aa791f61f163306651edc8baa40a249dcda777
FestersNephew <unclefestersnephew@gmail.com>
2023-05-19 12:37:08 -0400

    trying to get image to be in background

--- src/components/HeroSection/HeroSection.tsx
+++ src/components/HeroSection/HeroSection.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import styles from './HeroSection.module.css';
+import Feature1 from './../Feature1/Feature1';
+import Feature2 from './../Feature2/Feature2';
 
 const HeroSection: React.FC = () => {
   return (
-    <div className={styles.heroSection}>
-      {/* Content to be overlaid on top of the background image */}
-    </div>
+    <section className={styles.heroSection}>
+      <div className={styles.featureContainer}>
+        <Feature1 />
+        <Feature2 />
+      </div>
+      
+    </section>
   );
 };
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import Head from 'next/head';
 import { useEffect } from 'react';
-import Feature1 from '../components/Feature1/Feature1';
-import Feature2 from '../components/Feature2/Feature2';
 import Feature3 from '../components/Feature3/Feature3';
 import Header from '../components/Header/Header';
 import TextBoxSection from '../components/TextBoxSection/TextBoxSection';
 import Footer from '../components/Footer/Footer';
+import HeroSection from '@/components/HeroSection/HeroSection';
 
 
 const HomePage: React.FC = () => {
@@ -18,22 +17,11 @@ const HomePage: React.FC = () => {
         <title>Myco-Net | Decentralized AI Development Community</title>
       </Head>
       <Header />
-      <div
-        className="background"
-        style={{
-          backgroundImage: `url("/images/tree.png")`, // Update the image path
-          zIndex: 1,
-          backgroundSize: 'cover',
-          backgroundPosition: 'center',
-        }}
-      />
+      <HeroSection />
+      
       <div className="container">
         <main>
           <TextBoxSection />
-          <div className="feature-container">
-            <Feature1 />
-            <Feature2 />
-          </div>
           <div className="feature3-container">
             <Feature3 />
           </div>
